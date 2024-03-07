289f148266ee7cd593cb9f8f929921af3d063a69
Jose Luis <joseluis_c_garcia@hotmail.com>
2024-03-07 02:20:04 +0100

    AMPLIA Coding Test First Commit, basic structure

---
+++ src/main/java/es/amplia/microstreaminganalytics/MicroStreamingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package es.amplia.microstreaminganalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroStreamingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroStreamingAnalyticsApplication.class, args);
+	}
+
+}
